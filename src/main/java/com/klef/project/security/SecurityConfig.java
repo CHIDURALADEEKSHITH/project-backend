1ca2bca5c98116288cea6da2fb88b2918cc28b29
CHIDURALADEEKSHITH <2400030364@kluniversity.in>
2026-05-26 12:23:50 +0530

    updated cros origin

--- src/main/java/com/klef/project/security/SecurityConfig.java
+++ src/main/java/com/klef/project/security/SecurityConfig.java
@@ -61,7 +61,10 @@ public class SecurityConfig
     {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of(
+        	    "http://localhost:5173",
+        	    "https://bhagyalaxmifertilizers.netlify.app"
+        	));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
